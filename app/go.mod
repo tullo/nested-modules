c2be454831d9d559562da6374ac992c8ec69841e
Andreas Amstutz <tullo@users.noreply.github.com>
2023-02-05 00:45:00 +0100

    add app module

---
+++ app/go.mod
@@ -0,0 +1,5 @@
+module github.com/tullo/nested-modules/app
+
+go 1.19
+
+require github.com/tullo/nested-modules/lib v0.0.1
---
+++ app/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/tullo/nested-modules/lib"
+)
+
+// Simple application
+func main() {
+	fmt.Println(lib.CurrentTime())
+}
